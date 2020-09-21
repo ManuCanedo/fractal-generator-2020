38602b118fb682fc7a190ff9f66b532c84649c2b
Manu Canedo <chapi.gal@gmail.com>
2020-09-21 12:18:36 +0100

    Implemented building project through PREMAKE

---
+++ GenerateProject.bat
@@ -0,0 +1,2 @@
+call vendor\premake\bin\premake5.exe vs2019
+PAUSE
---
+++ premake5.lua
@@ -0,0 +1,47 @@
+workspace "fractal-generator"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "fractal-generator"
+    location ""
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "src/**.h",
+        "src/**.cpp"
+    }
+
+    includedirs
+    {
+        "vendor/spdlog/include",
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        defines "FRACTAL_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "FRACTAL_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "FRACTAL_DIST"
+        optimize "On"
