6352ba62ddfa32b199df7949c906743a6b297888
Manu Canedo <chapi.gal@gmail.com>
2021-03-17 00:43:55 +0000

    Update Bitmap.h

--- src/Bitmap.h
+++ src/Bitmap.h
@@ -8,7 +8,6 @@ namespace Fractal
 	{
 	public:
 		Bitmap(const int width, const int height);
-		~Bitmap() = default;
 
 		void SetPixel(uint64_t x, uint64_t y, uint8_t red, uint8_t green, uint8_t blue);
 		bool Write(std::string filename);
